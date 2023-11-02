8589e64871c02ac009c26e718e679f601cab1777
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-02 18:54:11 +0300

    project finish

--- CoroutinesWithRetrofit/app/src/main/java/com/example/coroutineswithretrofit/model/Country.kt
+++ CoroutinesWithRetrofit/app/src/main/java/com/example/coroutineswithretrofit/model/Country.kt
@@ -1,7 +1,12 @@
 package com.example.coroutineswithretrofit.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Country(
+    @SerializedName("name")
     val countryName: String?,
+    @SerializedName("capital")
     val capital: String?,
+    @SerializedName("flagPNG")
     val flag: String?
 )
