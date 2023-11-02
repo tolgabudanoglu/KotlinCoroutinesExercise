d85a0ea141feede9c7ab1b6bcef4147cc7b7f611
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-02 23:54:09 +0300

    project setup

--- CoroutinesWithRoom/app/src/main/java/com/example/coroutineswithroom/MainActivity.kt
+++ CoroutinesWithRoom/app/src/main/java/com/example/coroutineswithroom/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.coroutineswithroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
     }
 }
---
+++ CoroutinesWithRoom/app/src/main/java/com/example/coroutineswithroom/viewmodel/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.coroutineswithroom.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    val userDeleted = MutableLiveData<Boolean>()
+    val signout = MutableLiveData<Boolean>()
+
+    fun onSignout() {
+
+    }
+
+    fun onDeleteUser() {
+
+    }
+
+}
