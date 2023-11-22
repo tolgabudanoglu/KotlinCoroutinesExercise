ac6f4b28358d68c56f844c86e91583db74b74df9
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-22 17:37:12 +0300

    AsynchronousFlow

---
+++ AsynchronousFlow2/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+}
