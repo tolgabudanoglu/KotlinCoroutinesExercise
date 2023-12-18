15fe6b7b9caef3c613475be6e7053a2a8b656b7c
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-12-18 23:42:18 +0300

    channels

---
+++ Channels/app/src/main/java/com/example/channels/Producer.kt
@@ -0,0 +1,17 @@
+package com.example.channels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        produceSquares().consumeEach { println(it) }
+    }
+}
+
+fun CoroutineScope.produceSquares() = produce {
+    for(x in 1..5)
+        send(x * x)
+}
